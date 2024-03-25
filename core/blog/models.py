663e2e1a4f2ddfe8f8e05b4b6252d3ca5384b590
Ben <146076866+Benfoxyy@users.noreply.github.com>
2024-03-25 22:47:35 +0330

    create 2 applications and their models

---
+++ core/blog/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+class Post(models.Model):
+    author = models.ForeignKey(User,on_delete=models.CASCADE)
+    image = models.ImageField(null=True, blank=True)
+    title = models.CharField(max_length=250)
+    content = models.TextField()
+    status = models.BooleanField()
+    category = models.ForeignKey("Category", on_delete=models.SET_NULL)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
