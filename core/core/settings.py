d05a948a9a677a1de276a07d59db55cbc58aa98f
Ben <146076866+Benfoxyy@users.noreply.github.com>
2024-03-26 20:36:55 +0330

    create base form of user

--- core/core/settings.py
+++ core/core/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "blog",
+    # "blog",
     "accounts",
 ]
 
@@ -124,3 +124,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#config my base user model
+AUTH_USER_MODEL = 'accounts.User'
