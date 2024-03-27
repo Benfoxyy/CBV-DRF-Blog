74c62cc302b2f2bd70652af97102decee8d8a0c1
Ben <146076866+Benfoxyy@users.noreply.github.com>
2024-03-27 14:25:55 +0330

    base admin setting

--- core/accounts/admin.py
+++ core/accounts/admin.py
@@ -1,3 +1,46 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    model = User
+
+    list_display = ('email','is_superuser','is_active',)
+    list_filter = ("email", "is_staff", "is_active",)
+    ordering = ('email',)
+    search_fields = ('email',)
+
+    fieldsets = (
+        ("Authentication", {
+            "fields": (
+                'email','password'
+            ),
+        }),
+        ("Permission", {
+            "fields":(
+                "is_superuser", "is_staff", "is_active",
+            ),
+        }),
+        ("Group Permission", {
+            "fields":(
+                "groups", "user_permissions",
+            ),
+        }),
+        ("Important_date", {
+            "fields":(
+                "last_login",
+            ),
+        }),
+    )
+    
+    add_fieldsets = (
+        ("Form", {
+            "classes": ("wide",),
+            "fields": (
+                "email", "password1", "password2", "is_staff",
+                "is_active","is_superuser",
+            )}
+        ),
+    )
+
+admin.site.register(User,CustomUserAdmin)
--- core/accounts/models.py
+++ core/accounts/models.py
@@ -31,7 +31,7 @@ class User(AbstractBaseUser,PermissionsMixin):
     email = models.EmailField(max_length=255,unique=True)
     is_superuser = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
     # is_verified = models.BooleanField(default=False)
     USERNAME_FIELD = "email"
     # REQUIRED_FIELDS = []
