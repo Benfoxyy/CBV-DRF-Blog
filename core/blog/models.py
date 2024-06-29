14f9487d8330ad6f67c467d73f028c893baa24bc
Ben <146076866+Benfoxyy@users.noreply.github.com>
2024-06-29 19:27:38 +0330

    aeffae

--- core/blog/models.py
+++ core/blog/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from accounts.models import Profile
 
+class Comments(models.Model):
+    author = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True)
+    post = models.ForeignKey("Post", on_delete=models.SET_NULL, null=True)
+    content = models.CharField(max_length=250)
+
+    def __str__(self):
+        return f"{self.post} - {self.content}"
 
 class Comments(models.Model):
     author = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True)
