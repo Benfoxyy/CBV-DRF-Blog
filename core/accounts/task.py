936e4d3b7c4198b55a2f85b3d9f9f6ee25cee6c5
Ben <146076866+Benfoxyy@users.noreply.github.com>
2024-10-10 15:48:29 +0330

    test

--- core/accounts/task.py
+++ core/accounts/task.py
@@ -1,11 +1,8 @@
 from celery import shared_task
 from mail_templated import EmailMessage
-from time import sleep
-
 
 @shared_task
 def SendEmail(token, email):
-    sleep(10)
     EmailMessage(
         "email/verify.tpl",
         {"token": token},
